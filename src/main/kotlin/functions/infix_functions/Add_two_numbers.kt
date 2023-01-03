98b75c23898d9580a93397692e8005b8352ef2e1
KenStarry <starrycodes@gmail.com>
2023-01-03 23:14:02 +0300

    Infix function to add two numbers

---
+++ src/main/kotlin/functions/infix_functions/Add_two_numbers.kt
@@ -0,0 +1,14 @@
+package functions.infix_functions
+
+
+//  function to add two numbers
+infix fun Int.plus(num: Int): Int {
+    return this + num
+}
+
+fun main() {
+
+    val addedNums = 15 plus 20
+    println("15 + 20 = $addedNums")
+
+}
