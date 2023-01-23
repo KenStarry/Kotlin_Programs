1edd2181ed5e002f18016fe1aa91eced7863b4d3
KenStarry <starrycodes@gmail.com>
2023-01-23 21:37:39 +0300

    Kotlin typealias

--- src/main/kotlin/classes/TypeAliases.kt
+++ src/main/kotlin/classes/TypeAliases.kt
@@ -1,2 +1,26 @@
 package classes
 
+typealias bool = Boolean
+typealias onPrint = (message: String) -> Unit
+
+fun sayHello(
+    isLegible: bool,
+    onPrint: onPrint
+) {
+   if (isLegible)
+       onPrint("Hello there stranger")
+}
+
+fun main() {
+
+    println("Enter age : ")
+    val age = readln().toInt()
+
+    if (age > 18)
+        sayHello(
+            isLegible = true,
+            onPrint = { println(it) }
+        )
+    else
+        println("You are too young!")
+}
